032b14d4cce8d78818a0db1cdf89e85780286ae5
icaro moroni <icaromorone@gmail.com>
2023-03-01 23:47:08 -0300

    alterações nos arquivos do diretorio web

--- web-api-filmes/comportamento.js
+++ web-api-filmes/comportamento.js
@@ -2,13 +2,42 @@ baseURL= 'http://127.0.0.1:8000/filmes'
 
 let filmes=[]
 
-functionatualizar_tela(){
+function atualizar_tela(){
     //manipulacao de DOM
-    const ul_filmes = document.getElementById('list-fime')
+    const ul_filmes = document.getElementById('list-filmes')
 
     for(let filme of filmes){
         const item = document.createElement('li')
-        item.innerText = filme.nome
+        //const label = '#' + filme.id + '-' + filme.nome + '-' + filme.genero 
+        const label = `#${filme.id} - ${filme.nome} - ${filme.genero} `
+
+        const btn_editar = document.createElement('a') //cria tag <a></a> no arquivo.html
+        btn_editar.innerText = 'Editar' // cria um test dentro da tag a <a>Editar</a>
+        btn_editar.href = '#' //caminho do link da tag a
+        btn_editar.onclick = () => {alert('Editar')} //dentro das chaves pode colocar o que precisa fazer quando dar um click
+
+        const btn_remover = document.createElement('a') //cria tag <a></a> no arquivo.html
+        btn_remover.innerText = 'Remover' // cria um test dentro da tag a <a>Remover</a>
+        btn_remover.href = '#' //caminho do link da tag a
+        const espaco = document.createElement('span')
+        espaco.innerText = ' '
+        btn_remover.onclick = async () => {
+            //alert(`Remover o Filme ${filme.nome}!!`)
+            //chama a API metodo DELETE passando o ID URL
+            const response = await fetch(baseURL+'/'+filme.id, {method:'DELETE'})
+            
+            // se deu certo..
+            if(response.ok){
+                alert('Filme removido com sucesso!')
+                carregar_filmes()
+            }
+        }
+        
+
+        item.innerText = label
+        item.appendChild(btn_editar)
+        item.appendChild(espaco)
+        item.appendChild(btn_remover)
 
         ul_filmes.appendChild(item)
     }
@@ -22,6 +51,8 @@ async function carregar_filmes(){
     filmes = await response.json()
 
     atualizar_tela()
+    //console.log('Status', status)
+    //console.log('Dados', dados)
 }
 
 function configurar_formulario(){
@@ -32,20 +63,21 @@ function configurar_formulario(){
         const dados = form_filme.children
         const nome = dados[0].value
         const genero = dados[1].value
-        const ano = dados[2].value
-        const duracao = input_duracao.value
+        const ano = Number(dados[2].value)
+        const duracao = Number(input_duracao.value)
 
         const filme = {nome, genero, ano, duracao}
 
         console.log('submenteu!')
         
         const response = await fetch(baseURL, {
-            method:'POST',
-            body: JSON.stringify(filme),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
+                                                method:'POST',
+                                                body: JSON.stringify(filme),
+                                                headers: {
+                                                    'Content-Type': 'application/json'
+                                                }
+                                            }
+                                    )
         if(response.status === 201){
             alert('Filme Adicionadado com sucesso!')
         }else{
